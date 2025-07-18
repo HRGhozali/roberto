7560c17b6327ba7dfeccb4645c13e26b83c2079b
rfletez72 <rfletez@gmail.com>
2025-07-18 15:15:46 -0500

    20250718 v2

--- src/api/users/getList.js
+++ src/api/users/getList.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check, validationResult } = require('express-validator');
-const { Sequelize, Op } = require('sequelize');
-const { GetReqValues } = require('../../utils/utils');
+const { QueryTypes } = require('sequelize');
+const { GetReqValues, IsNulo } = require('../../utils/utils');
 // const {
 //   authToken,
 // } = require('../../utils/middleware');
@@ -40,116 +40,152 @@ module.exports = () => {
 
   mRouters
     .route('/')
-    .post([check('search'), check('getActive').isBoolean()], async (req, res) => {
-      const errors = validationResult(req);
-      let gg = require('../../utils/myglobal');
-      if (!errors.isEmpty())
-        return res
-          .status(200)
-          .json(
-            gg.returnDat(true, 400, 'API required values.', errors.array())
-          );
-      console.log('user:', req.user);
-      const mDat = GetReqValues(req);
-      let { search, getActive } = mDat;
-      if (req.user['role'] > 3 || req.user['role'] < 1) {
-        return res
-          .status(200)
-          .json(
-            gg.returnDat(
-              true,
-              400,
-              'Your role cannot get a list of users.',
-              null
-            )
-          );
-      }
-      let searchCheck = '%' + search + '%';
-      if (search == null || !search || search == '') {
-        console.log('yes');
-        searchCheck = '(.*?)';
-      }
-      let activeCheck = '%';
-      if (!getActive) {
-        activeCheck = '%1%';
-      }
+    .post(
+      [
+        check('search'),
+        check('getActive').isBoolean()
+      ], async (req, res) => {
+        const errors = validationResult(req);
+        let gg = require('../../utils/myglobal');
+        if (!errors.isEmpty())
+          return res
+            .status(200)
+            .json(
+              gg.returnDat(true, 400, 'API required values.', errors.array())
+            );
+        console.log('user:', req.user);
+        const mDat = GetReqValues(req);
+        let { search, getActive } = mDat;
+        if (req.user['role'] > 3) {
+          return res
+            .status(200)
+            .json(
+              gg.returnDat(
+                true,
+                400,
+                'Your role cannot get a list of users.',
+                null
+              )
+            );
+        }
 
-      const dat = await global.Models.users  // It returns nothing when Search is empty. Still working on it.
-        .findAll({
-          attributes: ['id', 'email'],
-          where: {
-            [Op.and]: [
-              {
-                [Op.or]: [
-                  {
-                    firstName: {
-                      [Op.like]: searchCheck,
-                    },
-                  },
-                  {
-                    lastName: {
-                      [Op.like]: searchCheck,
-                    },
-                  },
-                  {
-                    email: {
-                      [Op.like]: searchCheck,
-                    },
-                  },
-                  Sequelize.where(Sequelize.fn('concat', Sequelize.col('firstName'), Sequelize.col('lastName')), {
-                    [Op.like]: searchCheck,
-                  }),
-                ],
-                active: {
-                  [Op.like]: activeCheck,
-                },
-              },
-            ],
+        if (IsNulo(search))
+          search = '';
+        search = search.replace(/'/g, '');
+
+
+        // const dat = await global.Models.users  // It returns nothing when Search is empty. Still working on it.
+        //   .findAll({
+        //     attributes: ['id', 'email'],
+        //     where: {
+        //       [Op.and]: [
+        //         {
+        //           [Op.or]: [
+        //             {
+        //               firstName: {
+        //                 [Op.like]: searchCheck,
+        //               },
+        //             },
+        //             {
+        //               lastName: {
+        //                 [Op.like]: searchCheck,
+        //               },
+        //             },
+        //             {
+        //               email: {
+        //                 [Op.like]: searchCheck,
+        //               },
+        //             },
+        //             Sequelize.where(Sequelize.fn('concat', Sequelize.col('firstName'), Sequelize.col('lastName')), {
+        //               [Op.like]: searchCheck,
+        //             }),
+        //           ],
+        //           active: {
+        //             [Op.like]: activeCheck,
+        //           },
+        //         },
+        //       ],
+        //     },
+        //   }) // Assuming we only wanna be able to search active users.
+        //   .then(async function (data) {
+        //     let json = {};
+        //     if (data == null || !data) {
+        //       json = {
+        //         error: true,
+        //         code: 400,
+        //         message: 'Invalid data/session.',
+        //         data: null,
+        //       };
+        //     } else if (data.size === 0) {
+        //       json = {
+        //         error: false,
+        //         code: 200,
+        //         message: 'No users to return.',
+        //         data: null,
+        //       };
+        //     } else {
+        //       try {
+        //         json = {
+        //           error: false,
+        //           code: 200,
+        //           message: 'Successful.',
+        //           data: data,
+        //         };
+        //       } catch (searchError) {
+        //         let errmsg = searchError.message
+        //           ? searchError.message
+        //           : 'Error during search.';
+        //         json = { error: true, code: 400, message: errmsg, data: null };
+        //       }
+        //     }
+        //     return json;
+        //   })
+        //   .catch((error) => {
+        //     let errmsg = error.message
+        //       ? error.message
+        //       : 'Invalid request or deceptive request routing.';
+        //     return { error: true, code: 400, message: errmsg, data: null };
+        //   });
+
+        
+        let mSS = '';
+        mSS += ' Declare @search varchar(255) ';        
+        mSS += ' Declare @active bit ';        
+        mSS += ' set @search= :msearch ';        
+        mSS += ' set @active= :mactive ';
+        mSS += ' Select top 100 a.id,a.firstName,a.lastName,a.email,a.accessName,a.active ';
+        mSS += '  From users as a with(nolock) ';        
+        mSS += '  Where a.active=@active ';
+        if (search != '') {          
+          search = '%' + search + '%';
+          mSS += '  and ( a.firstName like @search or a.lastName like @search or a.email like @search or a.accessName like @search)';
+        }
+        const dat = await global.Models.dbo.query(mSS, {
+          replacements: {
+            msearch: search,
+            mactive: getActive
           },
-        }) // Assuming we only wanna be able to search active users.
-        .then(async function (data) {
-          let json = {};
-          if (data == null || !data) {
-            json = {
-              error: true,
-              code: 400,
-              message: 'Invalid data/session.',
-              data: null,
-            };
-          } else if (data.size === 0) {
-            json = {
-              error: false,
-              code: 200,
-              message: 'No users to return.',
-              data: null,
-            };
-          } else {
-            try {
-              json = {
-                error: false,
-                code: 200,
-                message: 'Successful.',
-                data: data,
-              };
-            } catch (searchError) {
-              let errmsg = searchError.message
-                ? searchError.message
-                : 'Error during search.';
-              json = { error: true, code: 400, message: errmsg, data: null };
-            }
-          }
-          return json;
+          raw: true,
+          type: QueryTypes.SELECT,
+          logging: console.log
         })
-        .catch((error) => {
-          let errmsg = error.message
-            ? error.message
-            : 'Invalid request or deceptive request routing.';
-          return { error: true, code: 400, message: errmsg, data: null };
-        });
+          .then(function (data) {
+            let json = {};
+            if (data.length == 0) {
+              json = { error: true, code: 404, message: 'Users not found.', data: null };
+            } else {
+              json = { error: false, code: 200, message: 'Successful.', data: data };
+            }
+            return json;
+          })
+          .catch((error) => {
+            let errmsg = error.message ? error.message : 'Invalid request or deceptive request routing.';
+            return { error: true, code: 404, message: errmsg, data: null };
+          });
 
-      if (dat.error == true) return res.status(200).json(dat);
-      else return res.status(200).json(dat);
-    });
+        if (dat.error == true) return res.status(200).json(dat);
+        else return res.status(200).json(dat);
+      });
 
   return mRouters;
 };
